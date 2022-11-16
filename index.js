3e255cce2cc68e319c29e19744daf1fcf5da2d36
thinkingmore <peterkevin823@gmail.comm>
2022-11-16 12:50:33 +0600

    created the doctors portal server

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.ltn8juo.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+app.get('/', async(req, res)=>{
+    res.send('doctors portal server is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`doctors portal server is running on ${port}`)
+})
