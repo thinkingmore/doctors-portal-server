bcd50cc09e076c719f4744d788174b30077ffac2
thinkingmore <peterkevin823@gmail.comm>
2022-11-16 17:00:52 +0600

    set path for getting appointment data,bookinng appointment and getting bookings data

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ltn8juo.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -24,12 +24,28 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const run = async() => {
     try{
         const appointmentOptionCollection = client.db("doctorsPortal").collection("AppointmentOptions")
-    
+        const bookingCollection = client.db("doctorsPortal").collection("bookings")
+        
         app.get("/appointmentOptions", async(req, res)=>{
             const query = {};
             const options = await appointmentOptionCollection.find(query).toArray();
             res.send(options);
         })
+
+        app.post("/bookings", async(req, res)=>{
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
+        app.get("/bookings", async(req, res)=>{
+            const query = {};
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
+
+
     }
     finally{
 
