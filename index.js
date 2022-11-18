b9d1d39caa69d780e89eed2923de56d69259ba9d
thinkingmore <peterkevin823@gmail.comm>
2022-11-18 13:01:12 +0600

    added jwt token creation functionality during user signup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -25,6 +26,7 @@ const run = async() => {
     try{
         const appointmentOptionCollection = client.db("doctorsPortal").collection("AppointmentOptions")
         const bookingsCollection = client.db("doctorsPortal").collection("bookings")
+        const usersCollection = client.db("doctorsPortal").collection("users")
         
         // collecting multiple data by aggregating query
         app.get("/appointmentOptions", async(req, res)=>{
@@ -68,13 +70,47 @@ const run = async() => {
             res.send(result);
         })
 
+        // app.get("/bookings", async(req, res)=>{
+        //     const query = {};
+        //     const result = await bookingsCollection.find(query).toArray();
+        //     res.send(result);
+        // })
+
         app.get("/bookings", async(req, res)=>{
-            const query = {};
-            const result = await bookingsCollection.find(query).toArray();
-            res.send(result);
+            const email = req.query.email;
+            const query = { email : email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+        // API for generating jwt token
+        app.get("/jwt", async(req,res)=>{
+            const email = req.query.email;
+            const query = {email: email};
+            const user = await usersCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN ,{expiresIn: "1h"});
+                return res.send({accessToken: token});
+            }
+            console.log(user);
+            res.status(403).send({accessToken: ''});
+           
         })
 
 
+        // post users data
+
+        app.get("/users", async(req, res)=>{
+            const query = {};
+            const result = await usersCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.post("/users", async(req, res)=>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+        
+            res.send(result);
+        })
     }
     finally{
 
