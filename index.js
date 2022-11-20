120faee6c1b3b825df1d07029fd4873da22293c3
thinkingmore <peterkevin823@gmail.comm>
2022-11-20 12:16:39 +0600

    added path for checking if a user is admin or not

--- index.js
+++ index.js
@@ -136,6 +136,14 @@ const run = async() => {
             res.send(result);
         })
 
+        app.get('/users/admin/:email',async(req,res)=>{
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({isAdmin: user?.role === 'admin' });
+
+        })
+
         app.put('/users/admin/:id',verifyJWT, async(req, res)=>{
             const decodedEmail = req.decoded.email;
             const query = { email: email}
